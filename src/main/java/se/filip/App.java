6571e9861a1f4334cf2b1337ddd584b0d2338adf
Filip Mathsson <pigfof@gmail.com>
2021-10-18 16:11:20 +0200

    more tasks

--- src/main/java/se/filip/App.java
+++ src/main/java/se/filip/App.java
@@ -1,9 +1,5 @@
 package se.filip;
-
 import java.util.ArrayList;
-
-
-
 public final class App {
     private App() {
     }
@@ -13,9 +9,26 @@ public final class App {
      * 
      * @param args The arguments of the program.
      */
+    
     public static void main(String[] args) {
-        ColorCheck colorcheck = new ColorCheck();
-        colorcheck.run();
-        
+        ArrayList<Integer> talLista = new ArrayList<>();
+        System.out.println("mata in tal");
+        int tal = Integer.parseInt(System.console().readLine());
+        talLista.add(tal);
+        while (true) {
+            System.out.println("Vill du mata in fler?");
+            String answer = System.console().readLine();
+            if (answer.equalsIgnoreCase("j")||answer.equalsIgnoreCase("y")|| answer.equalsIgnoreCase("yes")|| answer.equalsIgnoreCase("ja")) {
+                tal = Integer.parseInt(System.console().readLine());
+                talLista.add(tal);
+            }
+            else{
+                for (int i = 0; i < talLista.size(); i+=2) {
+                    //Replace code
+                }
+                break;
+            }
+        }
     }
+    
 }
